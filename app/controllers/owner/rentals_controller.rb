7029ef637bd2d148bc350884b3a09a08d1286f86
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-02-21 19:52:39 +0100

    all controllers

--- app/controllers/owner/rentals_controller.rb
+++ app/controllers/owner/rentals_controller.rb
@@ -1,2 +1,21 @@
 class Owner::RentalsController < ApplicationController
+  before_action :set_rental, only: [:accept, :decline]
+
+  def index
+    @rentals = Rental.all
+  end
+
+  def accept
+    @rental.status = "accepted"
+  end
+
+  def decline
+    @rental.status = "declined"
+  end
+
+  private
+
+  def set_rental
+    @rental = Rental.find(params[:id])
+  end
 end
--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,19 @@
 class RentalsController < ApplicationController
+  def index
+    @rentals = Rental.all
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.status = "pending"
+    @rental.save
+
+    redirect_to rentals_path
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:start_at, :end_at, :total_price, :status, :user_id, :bike_id)
+  end
 end
