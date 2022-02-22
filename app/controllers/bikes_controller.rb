ea0d0c4bd7f62c6bdf50e91651143a1be11c04f5
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-02-22 12:57:12 +0100

    show + delete pundit

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,10 +2,11 @@ class BikesController < ApplicationController
   before_action :set_bike, only: :show
 
   def index
-    @bikes = policy_scope(Bike)
+    @bikes = Bike.all
   end
 
   def show
+    @rental = Rental.new
   end
 
   # def new
@@ -13,7 +14,6 @@ class BikesController < ApplicationController
   # end
 
   # def create
-  #   authorize @bike
   #   @bike = Bike.new(bike_params)
   #   @bike.save
 
