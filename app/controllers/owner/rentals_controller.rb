4d49a9eab58923e4ff816fd7a55f9ac599767e3a
Herve Criaud <herve.criaud@ik.me>
2022-02-23 11:07:15 +0100

    matin

--- app/controllers/owner/rentals_controller.rb
+++ app/controllers/owner/rentals_controller.rb
@@ -3,7 +3,18 @@ class Owner::RentalsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @rentals = Rental.where(user_id: current_user)
+    # @rentals = Rental.where(user_id: current_user)
+    @owner_bikes = Bike.all.where(user_id: current_user)
+    @owner_rentals_canceled = []
+    @owner_rentals_accepted = []
+    @owner_rentals_pending = []
+    @owner_bikes.each do |bike|
+      bike.rentals.each do |bike_rental|
+        @owner_rentals_canceled << bike_rental if bike_rental.status == "Canceled"
+        @owner_rentals_accepted << bike_rental if bike_rental.status == "Accepted"
+        @owner_rentals_pending << bike_rental if bike_rental.status == "Pending"
+      end
+    end
   end
 
   def accept
