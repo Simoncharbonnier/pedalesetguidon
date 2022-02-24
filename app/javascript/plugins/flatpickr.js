ecdbf015b0055a7db8bc25a5e6b816edc02ad8d7
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-02-24 16:38:18 +0100

    PRICE

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,7 +3,8 @@ import flatpickr from "flatpickr";
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
     altInput: true,
-    minDate: "today"
+    minDate: "today",
+    defaultDate: "today"
   });
 }
 
